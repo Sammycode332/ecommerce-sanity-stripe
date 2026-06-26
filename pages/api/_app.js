14a80debed8e897e7fc62a893e9df609f3f88f5b
Sammycode332 <samueloroja123@gmail.com>
2026-06-26 19:30:35 +0100

    feat: refactor MyApp component to include a styled wrapper and header message

--- pages/api/_app.js
+++ pages/api/_app.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
-import { Layout } from '../components';
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }){
-    <Layout>
-        <Component {...pageProps} />
-    </Layout>
+function MyApp({ Component, pageProps }) {
+  return (
+    <div style={{ background: 'red', padding: '40px', color: 'white', zIndex: 9999 }}>
+      <h1>If you can see this, React is working!</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp
